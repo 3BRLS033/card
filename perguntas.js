983102c651c25c45d401afdc9f9f603f0f13f67e
3BRLS033 <00001106126634sp@al.educacao.sp.gov.br>
2024-11-24 22:22:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'Qual o resultado da conta 2+2?',
+    'O resultado é 4'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual estado está localizado Bragança Pta?',
+    'No estado de São Paulo'
+)
+
+criaCartao(
+    'Cultura Pop',
+    'Qual o nome do cachorro q sai com seus amigos pra resolver misterios?',
+    'Scooby doo'
+)
+
+criaCartao(
+    'Inglês',
+    'O q significa Hello?',
+    'Significa Oi ou Olá '
+)
